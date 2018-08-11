7e804f6876c1031db0d411a03f55a72b29ee4a4a
sid3github <siddharthpadwal3@gmail.com>
2018-08-11 15:30:04 +0530

    aysnc-await

--- index.js
+++ index.js
@@ -1,21 +1,67 @@
-//program for callback,async/await,promises.
-console.log('person1: shows ticket');
-console.log('person2: shows ticket');
+//<program for callback,async/await,promises.>
 
-const friendBringsTickets = new Promise(function(resolve,reject) {
-    setTimeout(function(err){
-        if(err){
-            throw err;
-        }
-        resolve('tickets');
-    }, 5000);
+// console.log('person1: shows ticket');
+// console.log('person2: shows ticket');
+
+// const friendBringsTickets = new Promise(function(resolve,reject) {
+//     setTimeout(function(err){
+//         if(err){
+//             throw err;
+//         }
+//         resolve('tickets');
+//     }, 5000);
         
-});
+// });
 
-friendBringsTickets.then(function(){
-    console.log('my ticket: show ticket');
-});
+// friendBringsTickets.then(function(){
+//     console.log('SID: show ticket');
+// });
 
-console.log('person3: shows ticket');
-console.log('person4: shows ticket');
+// console.log('person3: shows ticket');
+// console.log('person4: shows ticket');
+
+
+//<async - await>
+function wantToDrink (consume){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(err){
+            if(err){
+                throw err;
+            }
+            resolve(consume);
+        },5000);
+    });
+};
+
+async function ref(){
+    let consume = await wantToDrink('finally, yum!');
+    console.log(consume);
+};
+ref();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
